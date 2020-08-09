1761585eb9029d156e2717f219f8eba8fcbb2d39
himakhsc <hima.khsc@gmail.com>
2020-08-09 13:11:23 +0530

    Updates

---
+++ precache-manifest.ba6ab322eb5aaed705cb027a04f96142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea16ad07f70ffd2a0f0c418b48bb0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8157b056b01d82aa88d5",
+    "url": "/static/js/2.45472b5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45472b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d7967ce402e68a8ecd",
+    "url": "/static/js/main.28974ea9.chunk.js"
+  },
+  {
+    "revision": "a8c346b69f56ee86bc0e",
+    "url": "/static/js/runtime-main.a9e8824e.js"
+  }
+]);
